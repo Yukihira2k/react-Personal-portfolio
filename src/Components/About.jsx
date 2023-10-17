9ddfd4f49647d9bcd84a6c88dd547ff56843283e
Yukihira2k <89770909+Yukihira2k@users.noreply.github.com>
2023-10-17 19:11:05 +0800

    margins

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white ">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8 mt-[35%] md:mt-auto">
+            <div className="pb-8 mt-[45%] md:mt-auto">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500  ">About</p>
             </div>
 
